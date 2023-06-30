a57ba14dcd5168012bfc698ecaeb866077184da0
victor ortega <vorteg.r@gmail.com>
2023-06-30 16:00:11 -0600

    Adding Hero refactoring

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { siteConfig } from "@/config/site"
+
 import Image from 'next/image'
 import {
   Card,
@@ -7,6 +7,7 @@ import {
   CardHeader,
   CardTitle, Separator
 } from "@/components/ui"
+import Hero from '@/components/Hero'
 
 type Game = {
   id: number
@@ -28,12 +29,8 @@ export default async function IndexPage() {
   const games = await getGames()
   return (
     <>
-      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-        <h1 className="text-[2rem] font-semibold">{siteConfig.description}</h1>
-        <div className="flex flex-col items-center gap-2">
-          <Image src="https://res.cloudinary.com/dehsikb6h/image/upload/v1680214325/cachshoop/heros/store_gw3aew.png" width={1100} height={900} alt="cover" />
-        </div>
-      </section>
+      <Hero />
+
       <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
         <div className="flex">
           <div className="contents">
