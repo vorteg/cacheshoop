92cc059d9972e312cb17dd48d12a5df3fe701562
victor ortega <vorteg.r@gmail.com>
2023-07-12 19:00:58 -0600

    Components refactor using camel case

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,5 +2,5 @@ export * from './Button';
 export * from './Card';
 export * from './Separator';
 export * from './Icons';
-export * from './Tailwind-indicator'
+export * from './TailwindIndicator'
 
