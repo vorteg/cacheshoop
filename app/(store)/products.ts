1b651550747aa1c5893d13c72aef2b28ca98b4cb
victor ortega <vorteg.r@gmail.com>
2023-08-24 18:18:06 -0600

    Fix product.ts and encode dependency

--- app/(store)/products.ts
+++ app/(store)/products.ts
@@ -1,37 +1,37 @@
-// import { create } from 'zustand'
-// import { Product } from '@/types/product';
-// import dto from '@/supabase/client';
+import { create } from 'zustand'
+import { Product } from '@/types/product';
+import dto from '@/supabase/client_product';
 
-// // Definimos el tipo de estado
-// interface ProductsState {
-//   products: Product[];
-//   isLoading: boolean;
-//   apiCall:  () => Promise<void>; 
-// }
+// Definimos el tipo de estado
+interface ProductsState {
+  products: Product[];
+  isLoading: boolean;
+  apiCall:  () => Promise<void>; 
+}
 
-// // Creamos el store Zustand
-// const useProductsStore = create<ProductsState>((set) => ({
-//   products: [],
-//   isLoading: false,
-//   apiCall: async () => {
-//     try {
-//       set({ isLoading: true });
+// Creamos el store Zustand
+const useProductsStore = create<ProductsState>((set) => ({
+  products: [],
+  isLoading: false,
+  apiCall: async () => {
+    try {
+      set({ isLoading: true });
 
-//       const res = await dto();
-//        console.log("desde zustand")
-//       set((state) => {
-//         if (state.products.length === 0) {
-//           // console.log(res)
-//           return { products: res };
-//         }
-//         return state;
-//       });
-//     } catch (error) {
-//       console.error(error);
-//     } finally {
-//       set({ isLoading: false });
-//     }
-//   },
-// }));
+      const res = await dto();
+       console.log("desde zustand")
+      set((state) => {
+        if (state.products.length === 0) {
+          // console.log(res)
+          return { products: res };
+        }
+        return state;
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      set({ isLoading: false });
+    }
+  },
+}));
 
-// export default useProductsStore;
+export default useProductsStore;
