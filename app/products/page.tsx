2d2fcf5fc797d996610e86d0f550d1557a3d34a3
victor ortega <vorteg.r@gmail.com>
2023-08-31 12:45:52 -0600

    Adding Sidecart

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -6,6 +6,7 @@ import dto from "@/supabase/client_product"
 import { fetchfproductsAction, readfproductsAction } from '../(store)/storeFProducts/actions/fproductAction';
 import FilterGlob from '@/components/FilterGlob';
 import FilterProduct from '@/components/FilterProduct';
+import SideCart from '@/components/SideCart';
 
 //ToDOAgregar map para recorrer los productos
 export const dynamic = 'force-dynamic'
@@ -17,24 +18,24 @@ async function page() {
   return (
     <section className='grid grid-cols-1 md:grid-cols-[20%,auto] md:gap-5'>
       <FilterGlob />
+      {/* <SideCart /> */}
       <div className='hidden md:grid md:col-span-1'>
         <FilterProduct />
+
       </div>
 
 
-      <section className="grid grid-rows-[4%,auto] 
-       gap-5 md:grid-rows-[10%,auto] md:items-center">
-        <h1 className="row-span-1 text-3xl text-center 
-        max-w-full md:text-5xl">Productos disponibles</h1>
-        <section className="max-w-full flex flex-col gap-4">
+      <section className=" mx-auto max-w-2xl px-4 py-4 sm:px-6 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-bold tracking-tight">Productos disponibles</h2>
+        <section className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
           {
             products?.slice( 0, 4 ).map( ( item ) => (
               <CardProduct product={item} key={item.id} />
             ) )
           }
-          <Link href="#" className="text-blue-500 hover:underline">
+          {/* <Link href="#" className="text-blue-500 hover:underline">
             Siguiente
-          </Link>
+          </Link> */}
 
         </section>
 
--- components/CardProduct.tsx
+++ components/CardProduct.tsx
@@ -10,27 +10,32 @@ interface CardProductProps {
 
 const CardProduct: React.FC<CardProductProps> = ( { product, key } ) => {
   return (
-    <article key={key} className="bg-slate-300 rounded-lg md:flex md:flex-row">
+    <article key={key} className="group relative">
+      <Image
+        src={product.image}
+        alt={product.title}
+        className='aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80'
+        width={100}
+        height={100}
+        sizes='(max-width: 768px) 120px, 120px, (max-width: 1024px) 150px, 150px'
+      />
+      <div className='mt-4 flex justify-between'>
+        <div >
+          <h3 className="text-sm ">
+            <Link href={"/products/1"}>
+              <span aria-hidden="true" className="absolute inset-0"  ></span>
+              {product.title}
+            </Link>
 
-      <figure className='flex justify-center my-5 md:items-center md:w-1/4 '>
-        <Image
-          src={product.image}
-          alt={product.title}
-          className="w-24 h-24  overflow-hidden rounded-3xl"
-          width={100}
-          height={100}
-          sizes='(max-width: 768px) 120px, 120px, (max-width: 1024px) 150px, 150px'
-        />
-      </figure>
 
-      <div className='m-5 md:w-3/4'>
-        <h2 className="text-xl text-gray-700 font-semibold ">{product.title}</h2>
-        <p className="text-gray-600">
-          {product.description}
-        </p>
-        <p className="text-gray-600">Precio: <span className="text-green-500">${product.price}</span></p>
-
+          </h3>
+          <p className="mt-1 text-sm ">
+            black
+          </p>
+        </div>
+        <p className="text-sm font-medium ">${product.price}</p>
       </div>
+
 
 
     </article>
