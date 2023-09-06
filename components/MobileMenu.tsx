ce64e2a4f6e1cf028d046a9a5e4e11bd3e909230
victor ortega <vorteg.r@gmail.com>
2023-09-06 10:57:53 -0600

    UX Menu Mobil aplied

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -6,7 +6,7 @@ import { siteConfig } from "@/config/site"
 import { cn } from "@/lib/utils"
 import GhostButton from "./ui/GhostButton";
 import Image from "next/image";
-import { buttonVariants, Icons } from "@/components/ui"
+import { Button, buttonVariants, Icons } from "@/components/ui"
 import { ThemeToggle } from "@/components/ThemeToggle"
 
 interface MainNavProps {
@@ -15,76 +15,78 @@ interface MainNavProps {
 
 function MobileMenu() {
     const items = siteConfig.mainNav
-    const [Toggle, showMenu] = useState(false)
-    console.log(Toggle)
+    const [ Toggle, showMenu ] = useState( false )
+    console.log( Toggle )
     return (
-        <div className={Toggle? "fixed bottom-0 left-0 w-full h-[8rem] rounded-md border-t-2 dark:border-blue-900  dark:bg-black p-4 dark:text-white bg-gray-300 text-black text-center md:hidden" :"fixed bottom-0 left-0 w-full h-20 border-t-2 dark:border-blue-900  dark:bg-black p-4 dark:text-white bg-gray-300 text-black text-center md:hidden"}>
-                    {Toggle? (
-                        <nav className="grid grid-cols-[repeat(3,1fr)] gap-1">
-                            {items?.map(
-                                ( item, index ) =>
-                                    item.href && (
-                                        <GhostButton key={index.toString()}>
-                                            <Link
-                                                href={item.href}
-                                            >
-                                                {item.title}
-                                            </Link>
+        <div className={Toggle ? "fixed bottom-0 left-0 w-full h-[8rem] rounded-md border-t-2 dark:border-blue-900  dark:bg-black p-4 dark:text-white bg-gray-300 text-black text-center md:hidden" : "fixed bottom-0 left-0 w-full h-20 border-t-2 dark:border-blue-900  dark:bg-black p-4 dark:text-white bg-gray-300 text-black text-center md:hidden"}>
+            {Toggle ? (
+                <nav className="grid grid-cols-[repeat(3,1fr)] gap-1">
+                    {items?.map(
+                        ( item, index ) =>
+                            item.href && (
+                                <GhostButton key={index.toString()}>
+                                    <Link
+                                        href={item.href}
+                                    >
+                                        {item.title}
+                                    </Link>
 
-                                        </GhostButton>
-                                    )
-                            )}
-                            <div
-                                className={buttonVariants( {
-                                size: "sm",
-                                variant: "ghost",
-                                } )}
-                            >
-                                <Icons.car />
-                            </div>
-                            <div
-                                className={buttonVariants( {
-                                size: "sm",
-                                variant: "ghost",
-                                } )}
-                            >
-                                <Link href='/'>
-                                    <Icons.home />
-                                </Link>
-                            </div>
-                            <div
-                                onClick={() => showMenu(!Toggle)}
-                                className={buttonVariants( {
-                                size: "sm",
-                                variant: "ghost",
-                                } )}
-                            >
-                                <Icons.close className='absolute right-10 bottom-2' />
-                            </div>
-                        </nav>
-                    ) : null}
-                    <div className="flex space-x-12 justify-center">
-                        <div
-                            className={Toggle? 'hidden' : buttonVariants( {
+                                </GhostButton>
+                            )
+                    )}
+                    <div
+                        className={buttonVariants( {
                             size: "sm",
                             variant: "ghost",
-                            } )}
-                        >
-                            <Icons.car />
-                        </div>
-                        <Image
-                            src={siteConfig.images.logo}
-                            alt="Descripción de la imagen"
-                            className={Toggle? 'hidden': 'flex items-center'}
-                            onClick={() => showMenu(!Toggle)}
-                            width={44}
-                            height={38}
-                            quality="100"
-                        />
-                        <div className={Toggle? 'hidden': 'show'}>
-                            <ThemeToggle />
-                        </div>
+                        } )}
+                    >
+                        <Icons.car />
                     </div>
+                    <div
+                        className={buttonVariants( {
+                            size: "sm",
+                            variant: "ghost",
+                        } )}
+                    >
+                        <Link href='/'>
+                            <Icons.home />
+                        </Link>
+                    </div>
+                    <div
+                        onClick={() => showMenu( !Toggle )}
+                        className={buttonVariants( {
+                            size: "sm",
+                            variant: "ghost",
+                        } )}
+                    >
+                        <Icons.close className='absolute right-10 bottom-2' />
+                    </div>
+                </nav>
+            ) : null}
+            <div className="flex space-x-12 justify-center">
+                <div
+                    className={Toggle ? 'hidden' : buttonVariants( {
+                        size: "sm",
+                        variant: "ghost",
+                    } )}
+                >
+                    <Icons.car />
+                </div>
+                <Button className={Toggle ? 'hidden' : 'bg-gray-300  p-1 shadow-md dark:shadow-sm dark:shadow-white dark:bg-black'}>
+                    <Image
+                        src={siteConfig.images.logo}
+                        alt="Descripción de la imagen"
+                        className={Toggle ? 'hidden' : 'flex items-center'}
+                        onClick={() => showMenu( !Toggle )}
+                        width={44}
+                        height={38}
+                        quality="100"
+                    />
+                </Button>
+                <div className={Toggle ? 'hidden' : 'show'}>
+                    <ThemeToggle />
+                </div>
+            </div>
         </div>
     );
 };
