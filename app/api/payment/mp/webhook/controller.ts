3e5e4565504cd4488f2db3f884cc72800f39d167
victor ortega <vorteg.r@gmail.com>
2023-09-11 11:21:25 -0600

    webhook is working

--- app/api/payment/mp/webhook/controller.ts
+++ app/api/payment/mp/webhook/controller.ts
@@ -3,9 +3,10 @@ import {startMercadoPago } from '@/app/api/helpers/config-ml'
 export const receiveWebhook = async (payment:any) => {
   try {
     const mp = startMercadoPago()
-    console.log(payment)
     if(payment.type === 'payment'){
       const payId: number = parseInt(payment["data.id"], 10)
+      console.log("desde dentro de webhook")
+      console.log(payment)
       console.log(payId)
       const data = await mp?.payment.findById(payId)
       console.log(data)
@@ -13,6 +14,8 @@ export const receiveWebhook = async (payment:any) => {
     return 204
     
   } catch (error) {
+    if (error instanceof Error && error.message === 'Payment not found')
+      return 404
     console.log(error)
     return 500
   }
--- app/api/payment/mp/webhook/route.ts
+++ app/api/payment/mp/webhook/route.ts
@@ -7,14 +7,14 @@ export const dynamic = 'force-dynamic'
 export async function POST (request:Request) {
   
   const body = await request.json()
+  const requestUrl = new URL(request.url)
+  console.log(requestUrl.searchParams.toString())
   const code = await receiveWebhook(body)
 
-  console.log("desde webhook")
-  console.log(body)
-
-  return NextResponse.json({data:JSON.stringify({ data: "holi desde webhook" }), status:code})
-
-  // la plataforma web
+  console.log("desde webhook")  
+  if ( code === 404)
+    return NextResponse.json({data:'Payment not found', status:code})
+  return NextResponse.json({data:'webhook', status:code})
  
 
   
